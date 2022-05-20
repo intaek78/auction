151f7d47c2a21fc4db71a1e009fd5308cf8e5def
Ted <skcchwang@gmail.com>
2022-05-20 13:15:32 +0900

    deploy test

--- src/main/java/com/example/auction/SimpleCORSFilter.java
+++ src/main/java/com/example/auction/SimpleCORSFilter.java
@@ -18,11 +18,11 @@ import org.springframework.stereotype.Component;
   @Override public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
     HttpServletRequest request = (HttpServletRequest) req;
     HttpServletResponse response = (HttpServletResponse) res;
-    response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
-    response.setHeader("Access-Control-Allow-Credentials", "true");
-    response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
-    response.setHeader("Access-Control-Max-Age", "3600");
-    response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me");
+    //response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+    //response.setHeader("Access-Control-Allow-Credentials", "true");
+    //response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+    //response.setHeader("Access-Control-Max-Age", "3600");
+    //response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me");
     chain.doFilter(req, res); }
 
 }

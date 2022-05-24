22159ed7cc0715bd6cf0fa41f7c9d7584c7d60cf
Ted <skcchwang@gmail.com>
2022-05-24 18:55:29 +0900

    test

--- src/main/java/com/example/auction/domain/Bid.java
+++ src/main/java/com/example/auction/domain/Bid.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 package com.example.auction.domain;
 
 import com.example.auction.domain.service.Bidden;
@@ -165,3 +166,177 @@ public class Bid{
 
 
 }
+=======
+package com.example.auction.domain;
+
+import com.example.auction.domain.service.Bidden;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+
+import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Slf4j
+@Entity
+@Table(name="Bid_table")
+public class Bid{
+
+    private Long aucPostId;
+    private Long aucId;
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long bidId;
+    private Long bidId2;
+    private String status;
+    private Long bidMemId;
+    private Long bidAmount;
+    private Long sellerId;
+    private String crtDate;
+    private String title;
+    private String uptDate;
+    private String beAuctionedDate;
+    private String procGUBUN;
+    private String beAuctionedYN;
+    private String paymentReqYN;
+
+
+    @PostPersist
+    public void onPostPersist(){
+        Bidden bidden = new Bidden();
+        BeanUtils.copyProperties(this, bidden);
+        bidden.publishAfterCommit();
+
+        Bidden bidden2 = new Bidden();
+        BeanUtils.copyProperties(this, bidden2);
+        bidden2.publishAfterCommit();
+
+    }
+
+    @PostUpdate
+    public void onPostUpdate(){
+        log.info("==========%%%%%%%%%%%%Bid PostUpdate start=========");
+    }
+
+    public Long getAucId() {
+        return aucId;
+    }
+    public void setAucId(Long aucId) {
+        this.aucId = aucId;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Long getBidId2() {
+        return bidId2;
+    }
+    public void setBidId2(Long bidId2) {
+        this.bidId2 = bidId2;
+    }
+
+    public String getPaymentReqYN() {
+        return paymentReqYN;
+    }
+    public void setPaymentReqYN(String paymentReqYN) {
+        this.paymentReqYN = paymentReqYN;
+    }
+
+    public Long getAucPostId() {
+        return aucPostId;
+    }
+    public void setAucPostId(Long aucPostId) {
+        this.aucPostId = aucPostId;
+    }
+
+    public Long getBidId() {
+        return bidId;
+    }
+    public void setBidId(Long bidId) {
+        this.bidId = bidId;
+    }
+
+    public Long getBidMemId() {
+        return bidMemId;
+    }
+    public void setBidMemId(Long bidMemId) {
+        this.bidMemId = bidMemId;
+    }
+
+    public Long getSellerId() {
+        return sellerId;
+    }
+    public void setSellerId(Long sellerId) {
+        this.sellerId = sellerId;
+    }
+
+    public Long getBidAmount() {
+        return bidAmount;
+    }
+    public void setBidAmount(Long bidAmount) {
+        this.bidAmount = bidAmount;
+    }
+
+    public String getCrtDate() {
+        return crtDate;
+    }
+    public void setCrtDate(String crtDate) {
+        this.crtDate = crtDate;
+    }
+
+    public String getUptDate() {
+        return uptDate;
+    }
+    public void setUptDate(String uptDate) {
+        this.uptDate = uptDate;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBeAuctionedDate() {
+        return beAuctionedDate;
+    }
+    public void setBeAuctionedDate(String beAuctionedDate) {
+        this.beAuctionedDate = beAuctionedDate;
+    }
+
+    public String getProcGUBUN() {
+        return procGUBUN;
+    }
+    public void setProcGUBUN(String procGUBUN) {
+        this.procGUBUN = procGUBUN;
+    }
+
+    public String getBeAuctionedYN() {
+        return beAuctionedYN;
+    }
+    public void setBeAuctionedYN(String beAuctionedYN) {
+        this.beAuctionedYN = beAuctionedYN;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+>>>>>>> Stashed changes
--- src/test/java/com/example/auction/domain/AucPaymentTest.java
+++ src/test/java/com/example/auction/domain/AucPaymentTest.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 package com.example.auction.domain;
 
 import com.example.auction.domain.repository.AucPaymentRepository;
@@ -52,4 +53,59 @@ class AucPaymentTest {
         Assertions.assertThat(aucPaymentRepository.count()).isEqualTo(1);
     }
 
+=======
+package com.example.auction.domain;
+
+import com.example.auction.domain.repository.AucPaymentRepository;
+import com.example.auction.domain.repository.AuctionRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class AucPaymentTest {
+
+    @Autowired
+    private AucPaymentRepository  aucPaymentRepository;
+
+    @Test
+    void onPostPersist() {
+        AucPayment aucPayment = new AucPayment();
+        aucPayment.setBidId(Long.parseLong("9999"));
+        aucPayment.setTitle("TestTitle");
+        aucPayment.setBidId2(Long.parseLong("9999"));
+        aucPayment.setAucPostId(Long.parseLong("1111"));
+        aucPayment.setAucId(Long.parseLong("11"));
+        aucPayment.setPaymentReqYN("Y");
+        aucPayment.setBeAuctionedDate("20220505");
+        aucPayment.setBeAuctionedYN("Y");
+        aucPayment.setBidAmount(Long.parseLong("50000"));
+        aucPayment.setBidMemId(Long.parseLong("5600"));
+        aucPayment.setCrtDate("20220504");
+        aucPayment.setProcGUBUN("END");
+        aucPayment.setSellerId(Long.parseLong("4000"));
+        aucPayment.setUptDate("20220606");
+        aucPayment.setPaymentGubun("AUCTION");
+        aucPayment.setPaymentResult("approve");
+
+        // when
+        AucPayment saveAucPayment = aucPaymentRepository.save(aucPayment);
+        // then
+        //Assertions.assertThat(auction).isSameAs(saveAuction);
+        Assertions.assertThat(aucPayment.getTitle()).isEqualTo(saveAucPayment.getTitle());
+        Assertions.assertThat(aucPayment.getBidId2()).isEqualTo(saveAucPayment.getBidId2());
+        Assertions.assertThat(aucPayment.getBidMemId()).isEqualTo(saveAucPayment.getBidMemId());
+        Assertions.assertThat(aucPayment.getCrtDate()).isEqualTo(saveAucPayment.getCrtDate());
+        Assertions.assertThat(aucPayment.getBidAmount()).isEqualTo(saveAucPayment.getBidAmount());
+        Assertions.assertThat(aucPayment.getUptDate()).isEqualTo(saveAucPayment.getUptDate());
+        Assertions.assertThat(aucPayment.getBeAuctionedYN()).isEqualTo(saveAucPayment.getBeAuctionedYN());
+        Assertions.assertThat(aucPayment.getSellerId()).isEqualTo(saveAucPayment.getSellerId());
+        Assertions.assertThat(saveAucPayment.getTitle()).isNotNull();
+        Assertions.assertThat(aucPaymentRepository.count()).isEqualTo(1);
+    }
+
+>>>>>>> Stashed changes
 }

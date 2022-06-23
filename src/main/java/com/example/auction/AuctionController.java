c54d63ac60194ce6a6599f321730694487e051f2
Ted <skcchwang@gmail.com>
2022-06-23 15:08:28 +0900

    hystrix

--- src/main/java/com/example/auction/AuctionController.java
+++ src/main/java/com/example/auction/AuctionController.java
@@ -1,7 +1,9 @@
 package com.example.auction;
 
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.web.bind.annotation.RestController;
 
+@EnableHystrix
 @RestController
  public class AuctionController {
 
